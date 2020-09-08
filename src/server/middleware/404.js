0856011c0c60c4e70723f4150df00631ec16d833
CHRIS HAMERSLY <CHRISTOPHERHAMERSLY@GMAIL.COM>
2020-09-07 17:51:23 -0700

    day-one

---
+++ src/server/middleware/404.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+module.exports = (req,res) => {
+  res.status(404);
+  res.statusMessage = 'Resource Not Found';
+  res.json({error:'Not Found'});
+};
---
+++ src/socket-server/socket-server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const io = require('../../index.js');
+require('socket-io');
+
+// const gameSpace = io.of('/game');
+
+console.log('in the socket server');
+
+// gameSpace.on('connection', (socket) => {
+//   console.log('socket', socket);
+//   socket.on('join', room => {
+//     console.log('joined', room);
+//     socket.join(room);
+//   });
+// });
+
+
+
